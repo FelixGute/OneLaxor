a95b9d5c24180752f92acf4200105183e53ec052
Felix Thålin <felix.thalin@guteskolan.se>
2024-03-07 19:33:00 +0100

    added filtering to the start screen so it only gets sessions with a date set to later than today-3 days

--- src/components/SessionListView.tsx
+++ src/components/SessionListView.tsx
@@ -5,10 +5,33 @@ import { SessionView } from "./SessionView";
 
 interface Props {}
 
+/*
+
+// Definiera det önskade datumet (till exempel två dagar tidigare än igår)
+
+
+// Hämta poster där date är efter det önskade datumet och done är 0
+const result = await db.myTable
+  .where("date")
+  .above(specificDate)
+  .and((item) => item.done === 0)
+  .sortBy("date");
+
+*/
+
 export function SessionListView({}: Props) {
+	const specificDate = new Date();
+	specificDate.setDate(specificDate.getDate() - 2);
+
 	// Sessions för att hämta alla sessions som är i listan
-	const sessions = useLiveQuery(() =>
-		db.sessionList.where({ done: 0 }).sortBy("time")
+	const sessions = useLiveQuery(
+		() =>
+			db.sessionList
+				.where("time")
+				.above(specificDate)
+				.and((item) => item.done === 0)
+				.sortBy("time")
+		// db.sessionList.where({ done: 0 }).sortBy("time")
 	);
 
 	if (!sessions) return null;
